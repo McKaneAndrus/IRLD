0c4f1e8919ba63a88590f6e473f5cf2cea840024
Michael Dennis <michael_dennis@cs.berkeley.edu>
2019-04-17 13:28:01 -0700

    got sacred roughly working, still need to seperate logging and visualization from the actual experiement

---
+++ experiments/frank_wolfe_train.py
@@ -0,0 +1,96 @@
+from sacred import Experiment
+from sacred.observers import FileStorageObserver
+from utils.data_utils import initialize_data_files, load_data
+import os
+from utils.tf_utils import os_setup
+import tensorflow as tf
+from utils.model_setup import create_tf_model
+from envs.environment_setup_utils import get_mdp
+from utils.demos_utils import get_demos
+
+ex = Experiment()
+ex.observers.append(FileStorageObserver.create('my_runs'))
+
+
+@ex.config
+def default_config():
+    mdp_num = 0
+
+    gamma = 0.99
+    alpha = 1e-4
+    beta1 = 0.9
+    beta2 = 0.999999
+    sq_td_err_penalty = 1
+    trans_penalty = 1
+    t_err_penalty = 1e0
+    q_err_penalty = 1e0
+    constraint_batch_size = 512
+
+    q_n_layers = 2
+    q_layer_size = 2048
+    q_activation = tf.nn.tanh
+    q_output_activation = None
+
+    invdyn_n_layers = 1
+    invdyn_layer_size = 256
+    invdyn_activation = tf.nn.relu
+    invdyn_output_activation = None
+
+    n_act_dim = 5
+
+    featurize_acts = lambda a: a
+    n_dirs = 5
+
+    # Boltz-beta determines the "rationality" of the agent being modeled.
+    # Setting it to higher values corresponds to "pure rationality"
+    boltz_beta = 50
+
+
+
+    #DEMO Config
+    gamma_demo = 0.99
+    n_demos = 200
+    demo_time_steps = 40
+    temp_boltz_beta = 50
+
+
+    #Frank Wolfe Config
+    batch_size = 200
+    n_training_iters = 1000000
+    MAX_ITER = 250
+    STOP_CRIT = 1e-6
+    num_tasks = 2
+
+@ex.automain
+def frank_wolfe_train(mdp_num, gamma, alpha, beta1, beta2, sq_td_err_penalty,
+             trans_penalty, t_err_penalty, q_err_penalty, constraint_batch_size, q_n_layers, q_activation,
+            q_output_activation, invdyn_n_layers, invdyn_layer_size, invdyn_output_activation, n_act_dim,
+            featurize_acts, n_dirs, boltz_beta, gamma_demo, temp_boltz_beta, n_demos, demo_time_steps,
+            n_training_iters, batch_size, num_tasks, MAX_ITER, STOP_CRIT):
+
+    os_setup()
+    data_dir = os.path.join('data', '1.1')
+    im_scope, q_scope, invsas_scope, invadt_scope = load_data(data_dir)
+    sess = tf.Session()
+
+    mdp = get_mdp(mdp_num)
+
+    _, _, frank_wolfe_train = create_tf_model(sess, mdp, q_scope, invsas_scope, invadt_scope,
+                                                                       gamma, alpha, beta1, beta2, sq_td_err_penalty,
+                                                                       trans_penalty,
+                                                                       t_err_penalty, q_err_penalty,
+                                                                       constraint_batch_size,
+                                                                       q_n_layers, q_activation, q_output_activation,
+                                                                       invdyn_n_layers,
+                                                                       invdyn_layer_size, invdyn_output_activation,
+                                                                       n_act_dim, featurize_acts,
+                                                                       n_dirs, boltz_beta)
+
+    constraints, nn_rollouts, train_idxes, val_demo_batch, true_qs, states, adt_samples = get_demos(mdp, gamma_demo,
+                                                                                                    temp_boltz_beta,
+                                                                                                    n_demos,
+                                                                                                    demo_time_steps)
+
+    frank_wolfe_train(sess, n_training_iters, nn_rollouts, train_idxes, batch_size, constraints, num_tasks,
+                      val_demo_batch, states, adt_samples, MAX_ITER, STOP_CRIT)
+
---
+++ utils/demos_utils.py
@@ -0,0 +1,69 @@
+import numpy as np
+from utils.learning_utils import nn_vectorize_rollouts, generate_constraints, get_rollout_indexes, sample_batch, featurize_states
+from utils.soft_q_learning import tabsoftq_gen_pol, tabsoftq_learn_Qs, vectorize_rollouts, generate_demonstrations
+
+
+def clean_demos(sas_obs, max_noops=15):
+    demo_example_idxes = list(range(len(sas_obs)))
+    unique_stays, stay_count = set([]), 0
+    for i,sas in enumerate(sas_obs):
+        sas = tuple(sas)
+        if sas[1] == 4:
+            stay_count += 1
+            if sas in unique_stays:
+                if stay_count > max_noops:
+                    demo_example_idxes.remove(i)
+            else:
+                unique_stays.add(sas)
+        else:
+            stay_count = 0
+    return demo_example_idxes
+
+
+def get_demos(mdp, gamma, temp_boltz_beta, n_demos, demo_time_steps):
+    exQs = tabsoftq_learn_Qs(mdp, gamma=gamma)
+    # The rationality constant used to generate demos could certainly vary from the one used in the model.....
+
+    demos = generate_demonstrations(mdp, tabsoftq_gen_pol(exQs * temp_boltz_beta), n_demos, demo_time_steps)
+    sas_obs, adt_obs = vectorize_rollouts(demos)
+
+    # Ensure bad areas have not been visited in exes (this is unique to this experiment)
+    demos = []
+    while len(demos) < n_demos:
+        new_demo = generate_demonstrations(mdp, tabsoftq_gen_pol(exQs, beta=50), 1, demo_time_steps)[0]
+        more_sas, more_adt = list(zip(*new_demo))
+        if len(set([adt[2] for adt in more_adt])) == 1:
+            demos += [new_demo]
+
+    sas_obs, adt_obs = vectorize_rollouts(demos)
+
+    good_indexes = clean_demos(sas_obs)
+    sas_obs, adt_obs = sas_obs[good_indexes], adt_obs[good_indexes]
+    constraints = generate_constraints(mdp)
+    nn_rollouts = nn_vectorize_rollouts(mdp, sas_obs, adt_obs)
+    train_idxes, val_idxes = get_rollout_indexes(sas_obs)
+    val_demo_batch = sample_batch(nn_rollouts, val_idxes)
+
+    # True q-vals for debugging and comparison purposes
+    Qs = tabsoftq_learn_Qs(mdp, gamma=gamma)
+    sa = np.transpose(
+        [np.tile(np.arange(mdp.num_states), mdp.num_actions), np.repeat(np.arange(mdp.num_actions), mdp.num_states)])
+    states, acts = sa[:, 0], sa[:, 1]
+    true_qs = Qs[states, acts]
+
+    # Preprocessing for training update visualizations
+    tts = np.arange(2)
+    acts = np.arange(mdp.num_actions)
+    adt_samples = np.transpose([np.tile(tts, len(acts)), np.repeat(acts, len(tts))])
+    adt_samples = adt_samples[adt_samples[:, 0].argsort()]
+    states = featurize_states(mdp, np.arange(mdp.num_states))
+
+    # Optional preloading of q-fn or dynamics models
+    # q_net_path = os.path.join(data_dir, 'true_q_16384.tf')
+    # save_tf_vars(sess, q_scope, q_net_path)
+    # load_tf_vars(sess, q_scope, q_net_path)
+
+    # invadt_path = os.path.join(data_dir, 'adt_fw_t.tf')
+    # load_tf_vars(sess, invadt_scope, invadt_path)
+
+    return constraints, nn_rollouts, train_idxes, val_demo_batch, true_qs, states, adt_samples
