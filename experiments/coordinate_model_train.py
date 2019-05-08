Fix god-awful, unstable neural architecture, add gradient clipping and experimental loss fns

experiments/coordinate_model_train.py
@@ -9,6 +9,7 @@ from utils.demos_utils import get_demos
 from utils.experiment_utils import current_milli_time
 from utils.models import InverseDynamicsLearner
 from envs.mars_map_gen import make_map, get_mdp_from_map
+import numpy as np
 
 coordinate_model_train_ex = Experiment("coordinate_model_train")
 coordinate_model_train_ex.observers.append(FileStorageObserver.create('logs/sacred'))
@@ -21,20 +22,20 @@ def default_config():
     mdp_map = get_tile_map(mdp_num)
 
     gamma = 0.99
-    alpha = 5e-4
+    alpha = 1e-2
     beta1 = 0.9
     beta2 = 0.999999
 
     constraint_batch_size = None
 
-    q_n_layers = 4
+    q_n_layers = 1
     q_layer_size = 128
     q_activation = tf.nn.relu
     q_output_activation = None
     target_update_freq = 50
 
     dyn_n_layers = 1
-    dyn_layer_size = 256
+    dyn_layer_size = 64
     dyn_activation = tf.nn.relu
     dyn_output_activation = None
 
@@ -53,19 +54,21 @@ def default_config():
 
 
     #Coordinate Config
-    batch_size = 200
+    batch_size = 64
     n_training_iters = 500000
-    dyn_pretrain_iters = 10000
-    horizon = 5000
-    alphas = [5e-3, 1e-4]
-    improvement_proportions = [-1, 0.1]
+    dyn_pretrain_iters = 20000
+    horizon = 2000
+    alphas = [5e-3, 1e-3] #[1e-4, 1e-4,1e-2,1e-4]
+    improvement_proportions = [0.1, -np.inf] #[0.1, -1, 0.1]
     switch_frequency = 500
     # Config made up of ['nall', 'ntll', 'tde', 'tde_sg_q', 'tde_sg_t']
     initial_update = None
-    update_progression = [[4],[1,2,5]]
-    model_save_weights = [1.0, 1.0, 1.0, 1.0, 0.0]
+    update_progression = [[1,4], [0,3]] #[[0],[5],[4],[7]] #[[4],[0,4,5]]
+    # update_weights = [[1.],[1.],[10000,1.]]
+    model_save_weights = [1.0, 1.0, 1.0, 0.0, 0.0, 0.0]
 
     tab_save_freq = 200
+    clip_global = True
 
     seed = 0
     gpu_num = 0
@@ -78,14 +81,15 @@ def coordinate_train(_run, mdp_map, gamma, alpha, beta1, beta2, constraint_batch
             q_output_activation, target_update_freq, dyn_n_layers, dyn_layer_size, dyn_activation, dyn_output_activation, boltz_beta, mellowmax,
             gamma_demo, temp_boltz_beta, n_demos, demo_time_steps, n_training_iters, dyn_pretrain_iters, batch_size,
             horizon, alphas, improvement_proportions, switch_frequency, initial_update, update_progression, model_save_weights, tab_save_freq,
-            gpu_num, seed):
+            clip_global, gpu_num, seed):
 
     os_setup(gpu_num)
     tf.reset_default_graph()
     data_dir = os.path.join('data', '1.1')
     # q_scope, dyn_scope = load_scopes(data_dir)
-    sess = tf.Session()
-
+    tf_config = tf.ConfigProto()
+    tf_config.gpu_options.allow_growth = True
+    sess = tf.Session(config=tf_config)
 
     mdp = get_mdp_from_map(mdp_map)
 
@@ -110,7 +114,8 @@ def coordinate_train(_run, mdp_map, gamma, alpha, beta1, beta2, constraint_batch
                          'initial_update': initial_update,
                          'update_progression':update_progression,
                          'model_save_weights': model_save_weights,
-                         'alphas': alphas}
+                         'alphas': alphas,
+                         'clip_global':clip_global}
 
         model.initialize_training_regime("coordinate", regime_params=regime_params)
 

experiments/mgda_model_train.py
@@ -20,13 +20,13 @@ def default_config():
     mdp_map = get_tile_map(mdp_num)
 
     gamma = 0.99
-    alpha = 5e-4
+    alpha = 1e-3
     beta1 = 0.9
     beta2 = 0.999999
 
     constraint_batch_size = None
 
-    q_n_layers = 4
+    q_n_layers = 1
     q_layer_size = 128
     q_activation = tf.nn.relu
     q_output_activation = None
@@ -34,7 +34,7 @@ def default_config():
 
 
     dyn_n_layers = 1
-    dyn_layer_size = 256
+    dyn_layer_size = 64
     dyn_activation = tf.nn.relu
     dyn_output_activation = None
 
@@ -56,7 +56,7 @@ def default_config():
     dyn_pretrain_iters = 10000
 
 
-    loss_configurations = [[0,3,5,6],[1,4,5,6]]
+    loss_configurations = [[1,4],[0,3]]
 
     tab_save_freq = 200
     seed = 0
@@ -73,8 +73,9 @@ def mgda_train(_run, mdp_map, gamma, alpha, beta1, beta2, constraint_batch_size,
     tf.reset_default_graph()
     data_dir = os.path.join('data', '1.1')
     # q_scope, dyn_scope = load_scopes(data_dir)
-    sess = tf.Session()
-
+    tf_config = tf.ConfigProto()
+    tf_config.gpu_options.allow_growth = True
+    sess = tf.Session(config=tf_config)
     mdp = get_mdp_from_map(mdp_map)
 
     mlp_params = {'q_n_layers':q_n_layers,

experiments/weighted_model_train.py
@@ -21,21 +21,21 @@ def default_config():
     mdp_map = get_tile_map(mdp_num)
 
     gamma = 0.99
-    alpha = 5e-4
+    alpha = 5e-3
     beta1 = 0.9
     beta2 = 0.999999
 
     constraint_batch_size = None
 
-    q_n_layers = 4
+    q_n_layers = 1
     q_layer_size = 128
     q_activation = tf.nn.relu
     q_output_activation = None
-    target_update_freq = 20
+    target_update_freq = 25
 
 
     dyn_n_layers = 1
-    dyn_layer_size = 256
+    dyn_layer_size = 64
     dyn_activation = tf.nn.relu
     dyn_output_activation = None
 
@@ -54,14 +54,15 @@ def default_config():
 
 
     #weighted Config
-    batch_size = 200
-    n_training_iters = 200000
-    dyn_pretrain_iters = 10000
+    batch_size = 64
+    n_training_iters = 400000
+    dyn_pretrain_iters = 20000
     # Config made up of ['nall', 'ntll', 'tde', 'tde_sg_q', 'tde_sg_t']
-    losses = [0,1,4,5]
-    loss_weights = [1.0, 1.0, 100.0, 1.0]
+    losses = [0,4,7]
+    loss_weights = [1.0, 1.0, 1.0]
 
     tab_save_freq = 500
+    clip_global = True
 
     seed = 0
     gpu_num = 0
@@ -73,14 +74,15 @@ def default_config():
 def weighted_train(_run, mdp_map, gamma, alpha, beta1, beta2, constraint_batch_size, q_n_layers, q_layer_size, q_activation,
             q_output_activation, target_update_freq, dyn_n_layers, dyn_layer_size, dyn_activation, dyn_output_activation, boltz_beta, mellowmax,
             gamma_demo, temp_boltz_beta, n_demos, demo_time_steps, n_training_iters, dyn_pretrain_iters, batch_size,
-            losses, loss_weights, tab_save_freq, gpu_num, seed):
+            losses, loss_weights, tab_save_freq, clip_global, gpu_num, seed):
 
     os_setup(gpu_num)
     tf.reset_default_graph()
     data_dir = os.path.join('data', '1.1')
     # q_scope, dyn_scope = load_scopes(data_dir)
-    sess = tf.Session()
-
+    tf_config = tf.ConfigProto()
+    tf_config.gpu_options.allow_growth = True
+    sess = tf.Session(config=tf_config)
 
     mdp = get_mdp_from_map(mdp_map)
 
@@ -99,7 +101,8 @@ def weighted_train(_run, mdp_map, gamma, alpha, beta1, beta2, constraint_batch_s
                                     mellowmax=mellowmax, alpha=alpha, beta1=beta1, beta2=beta2, seed=seed) #, q_scope=q_scope, dyn_scope=dyn_scope)
 
         regime_params = {"losses": losses,
-                         'loss_weights':loss_weights}
+                         'loss_weights':loss_weights,
+                         'clip_global':clip_global}
 
         model.initialize_training_regime("weighted", regime_params=regime_params)
 

utils/models.py
@@ -58,6 +58,10 @@ class InverseDynamicsLearner():
         # True Qs are only used for testing architectures and learning capabilities
         self.true_qs_ph = tf.placeholder(tf.float32, [None, n_act_dim], name="tqs")
 
+        demo_tile_t_one_hot = self.demo_tile_t_ph #tf.one_hot(tf.squeeze(self.demo_tile_t_ph, axis=1), mdp.tile_types, dtype=tf.int32)
+        constraint_tile_t_one_hot = self.constraint_tile_t_ph #tf.one_hot(tf.squeeze(self.constraint_tile_t_ph, axis=1), mdp.tile_types, dtype=tf.int32)
+
+
         demo_q_t = build_mlp(self.demo_obs_t_feats_ph,
                              n_act_dim, q_scope,
                              n_layers=q_n_layers, size=q_layer_size,
@@ -71,7 +75,7 @@ class InverseDynamicsLearner():
 
         self.neg_avg_act_log_likelihood = -tf.reduce_mean(act_log_likelihoods)
 
-
+        print(tf.shape(tf.concat((self.demo_act_t_ph, demo_tile_t_one_hot), axis=1)))
 
         dir_indexes = tf.concat([tf.expand_dims(tf.range(self.demo_batch_size_ph), 1), self.demo_dir_t_ph], axis=1)
 
@@ -81,13 +85,13 @@ class InverseDynamicsLearner():
 
         if adt:
             self.pred_obs = build_mlp(
-                tf.concat((self.demo_act_t_ph, self.demo_tile_t_ph), axis=1),
+                tf.concat((self.demo_act_t_ph, demo_tile_t_one_hot), axis=1),
                 n_dirs, dyn_scope,
                 n_layers=dyn_n_layers, size=dyn_layer_size,
                 activation=dyn_layer_activation, output_activation=dyn_output_activation)
         else:
             self.pred_obs = build_mlp(
-                tf.concat((self.demo_obs_t_feats_ph, self.demo_act_t_ph), axis=1),
+                tf.concat((demo_tile_t_one_hot, self.demo_act_t_ph), axis=1),
                 n_dirs, dyn_scope,
                 n_layers=dyn_n_layers, size=dyn_layer_size,
                 activation=dyn_layer_activation, output_activation=dyn_output_activation)
@@ -112,7 +116,7 @@ class InverseDynamicsLearner():
         # Predicted constraint next state given inv dyns
         if adt:
             constraint_pred_obs = build_mlp(
-                tf.concat((self.constraint_act_t_ph, self.constraint_tile_t_ph), axis=1),
+                tf.concat((self.constraint_act_t_ph, constraint_tile_t_one_hot), axis=1),
                 n_dirs, dyn_scope,
                 n_layers=dyn_n_layers, size=dyn_layer_size,
                 activation=dyn_layer_activation, output_activation=dyn_output_activation,
@@ -160,9 +164,6 @@ class InverseDynamicsLearner():
         target_t = self.constraint_rew_t_ph + gamma * tf.reduce_sum(constraint_next_vs, axis=1)
         self.td_err = tf.reduce_mean((constraint_q_t - target_t) ** 2)
 
-        #Experimental
-        self.ll_weighted_td_err = self.td_err * (self.neg_avg_trans_log_likelihood + self.neg_avg_act_log_likelihood)
-        self.td_err_weighted_ll = 1/(self.ll_weighted_td_err + 1e-8)
 
         # bellman residual penalty error with a stop gradient on transitions to prevent the bellman update from
         # 'hacking' the transition function to overly explain the demonstrations. This happens a lot early on before the
@@ -178,6 +179,14 @@ class InverseDynamicsLearner():
         constraint_next_vs_sgq = tf.multiply(tf.stop_gradient(constraint_v_tp1), constraint_pred_probs)
         target_t_sgq = self.constraint_rew_t_ph + gamma * tf.reduce_sum(constraint_next_vs_sgq, axis=1)
         self.td_err_sgq = tf.reduce_mean((tf.stop_gradient(constraint_q_t) - target_t_sgq) ** 2)
+
+        #Experimental
+        # self.best_ntll = 1.0
+        # self.delta_ll_td_err = self.td_err * self.neg_avg_act_log_likelihood * tf.minimum(self.neg_avg_trans_log_likelihood - self.best_ntll, 1e-12)
+        self.lqsafer = self.neg_avg_act_log_likelihood / (1000 - self.td_err + 1/self.neg_avg_trans_log_likelihood)
+        self.llt_weighted_td_err = self.td_err_sgq * self.neg_avg_trans_log_likelihood ** 5
+        self.lla_weighted_td_err = self.td_err * self.neg_avg_act_log_likelihood * self.neg_avg_trans_log_likelihood ** 5
+        # self.td_err_weighted_ll = 1/(self.ll_weighted_td_err + 1e-8)
 
         # Set up target network
         q_func_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=q_scope)
@@ -198,11 +207,11 @@ class InverseDynamicsLearner():
         self.true_q_err = tf.reduce_sum((self.true_qs_ph - self.test_qs_t)**2)
 
         self.loss_fns = np.array([self.neg_avg_act_log_likelihood, self.neg_avg_trans_log_likelihood,
-                         self.td_err, self.td_err_sgq, self.td_err_sgt, self.ll_weighted_td_err, self.td_err_weighted_ll]) #, self.true_q_err])
-        self.loss_fns_titles = np.array(['nall', 'ntll', 'tde', 'tde_sg_q', 'tde_sg_t', 'll_tde', 'tde_ll']) #, 'true_q_err'])
+                         self.td_err, self.td_err_sgq, self.td_err_sgt, self.llt_weighted_td_err, self.lqsafer, self.lla_weighted_td_err]) #, self.true_q_err])
+        self.loss_fns_titles = np.array(['nall', 'ntll', 'tde', 'tde_sg_q', 'tde_sg_t', 'llt_tde', 'lqsafe', 'lla_tde']) #, 'true_q_err'])
 
-        self.log_losses = self.loss_fns[[0,1,2,5,6]]
-        self.log_loss_titles = self.loss_fns_titles[[0,1,2,5,6]]
+        self.log_losses = self.loss_fns[[0,1,2,5,6,7]]
+        self.log_loss_titles = self.loss_fns_titles[[0,1,2,5,6,7]]
 
         self.regime = None
 
@@ -224,7 +233,13 @@ class InverseDynamicsLearner():
             self.weighted_loss = sum([self.loss_fns[losses[i]] * loss_weights[i] for i in range(len(losses))])
             self.log_losses = np.append(self.log_losses, [self.weighted_loss])
             self.log_loss_titles = np.append(self.log_loss_titles, ["weighted_loss"])
-            self.update = tf.train.AdamOptimizer(self.alpha, self.beta1, self.beta2).minimize(self.weighted_loss)
+            if regime_params['clip_global']:
+                optimizer = tf.train.AdamOptimizer(self.alpha, self.beta1, self.beta2)
+                gradients, variables = zip(*optimizer.compute_gradients(self.weighted_loss))
+                gradients, _ = tf.clip_by_global_norm(gradients, 5.0)
+                self.update = [optimizer.apply_gradients(zip(gradients, variables))]
+            else:
+                self.update = tf.train.AdamOptimizer(self.alpha, self.beta1, self.beta2).minimize(self.weighted_loss)
 
         if regime == "coordinate":
             # Coordinate descent training regime
@@ -237,8 +252,16 @@ class InverseDynamicsLearner():
             # Update progression is a list of list of ints in the range(len(self.loss_fns))
             self.loss_progression = [sum(self.loss_fns[config]) for config in regime_params["update_progression"]]
             #TODO Check for equivalent losses
-            self.update_progression = [tf.train.AdamOptimizer(self.alphas[i], self.beta1, self.beta2).minimize(
-                    loss, name="opt_{}".format(i)) for i,loss in enumerate(self.loss_progression)]
+            if regime_params['clip_global']:
+                self.update_progression = []
+                for i,loss in enumerate(self.loss_progression):
+                    optimizer = tf.train.AdamOptimizer(self.alpha, self.beta1, self.beta2)
+                    gradients, variables = zip(*optimizer.compute_gradients(loss))
+                    gradients, _ = tf.clip_by_global_norm(gradients, 5.0)
+                    self.update_progression += [optimizer.apply_gradients(zip(gradients, variables))]
+                else:
+                    self.update_progression = [tf.train.AdamOptimizer(self.alphas[i], self.beta1, self.beta2).minimize(
+                            loss, name="opt_{}".format(i)) for i,loss in enumerate(self.loss_progression)]
             if regime_params["initial_update"] is not None:
                 self.curr_update_index = -1
                 self.curr_loss = sum(self.loss_fns[regime_params["initial_update"]])
@@ -278,7 +301,9 @@ class InverseDynamicsLearner():
         assert(self.regime is not None)
 
         if dyn_pretrain_iters > 0:
-            temp_update = tf.train.AdamOptimizer(self.alpha, self.beta1, self.beta2).minimize(self.neg_avg_trans_log_likelihood)
+            temp_update1 = tf.train.AdamOptimizer(self.alpha, self.beta1, self.beta2).minimize(
+                self.neg_avg_trans_log_likelihood)
+            temp_update2 = tf.train.AdamOptimizer(self.alpha, self.beta1, self.beta2).minimize(self.td_err_sgt)
 
         tf.global_variables_initializer().run(session=self.sess)
 
@@ -317,16 +342,30 @@ class InverseDynamicsLearner():
 
         if dyn_pretrain_iters > 0:
             print("Pretraining the dynamics model with baseline method.")
-            for i in range(dyn_pretrain_iters):
+            for i in range(int(dyn_pretrain_iters/2)):
                 demo_batch = sample_batch(rollouts, train_idxes, batch_size)
                 feed_dict = self._get_feed_dict(demo_batch, constraints)
-                self.sess.run(temp_update, feed_dict=feed_dict)
+                self.sess.run(temp_update1, feed_dict=feed_dict)
 
                 if i % self.validation_freq == 0:
                     val_loss = self.sess.run(self.neg_avg_trans_log_likelihood, feed_dict=val_feed)
 
                 if i % 1000 == 0:
-                    print("{}: {}".format(i, val_loss))
+                    print("{} Transition Loss: {}".format(i, val_loss))
+            for i in range(dyn_pretrain_iters+1):
+                demo_batch = sample_batch(rollouts, train_idxes, batch_size)
+                feed_dict = self._get_feed_dict(demo_batch, constraints)
+                cqs, _ = self.sess.run([self.constraint_qs_t, temp_update2], feed_dict=feed_dict)
+
+                if i % self.validation_freq == 0:
+                    val_loss = self.sess.run(self.td_err_sgt, feed_dict=val_feed)
+
+                if i % target_update_freq == 0:
+                    self.sess.run(self.update_target_fn)
+
+                if i % 1000 == 0:
+                    print("{} Q-Learning Loss: {}".format(i, val_loss))
+                    print(np.max(cqs), np.min(cqs))
 
 
         try:
@@ -384,7 +423,7 @@ class InverseDynamicsLearner():
                 if train_time % tab_save_freq == 0 and train_time != 0:
                     # self.mdp only used in this section, can remove that dependency if we want
                     q_vals = self.sess.run([self.test_qs_t], feed_dict={self.test_q_obs_t_feats_ph: q_states})[0]
-                    print(np.max(q_vals), np.median(q_vals))
+                    print(train_time, np.max(q_vals), np.min(q_vals))
                     adt_logits = self.sess.run([self.pred_obs], feed_dict={self.demo_tile_t_ph: adt_samples[:,0][np.newaxis].T,
                                                                     self.demo_act_t_ph: adt_samples[:,1][np.newaxis].T})[0]
                     adt_probs = softmax(adt_logits)