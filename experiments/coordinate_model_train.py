Add KL Ball Scheduling and fix tabular method

experiments/coordinate_model_train.py
@@ -10,6 +10,7 @@ from utils.experiment_utils import current_milli_time
 from utils.models import InverseDynamicsLearner
 from envs.mars_map_gen import make_map, get_mdp_from_map
 import numpy as np
+from utils.learning_utils import logarithmic_schedule
 
 coordinate_model_train_ex = Experiment("coordinate_model_train")
 coordinate_model_train_ex.observers.append(FileStorageObserver.create('logs/sacred'))
@@ -173,11 +174,14 @@ def kl_boi():
     weight_norm = True
     mellowmax = None
     lse_softmax = None
+    n_training_iters = 200000
+    kl_ball_schedule = logarithmic_schedule(-1.0, -7.0, n_training_iters)
+    # kl_ball_schedule = lambda t: 2e-2
 
-    horizons = [500, 1000, 3000]
+    horizons = [500, 1000, 4000]
     switch_frequency = 500
     alphas = [5e-3, 1e-3, 5e-3]
-    improvement_proportions = [-np.inf, -np.inf, 0.25]  # [0.1, -1, 0.1]
+    improvement_proportions = [0.1, 0.1, 0.01]  # [0.1, -1, 0.1]
     # Config made up of ['nall', 'ntll', 'tde', 'tde_sg_q', 'tde_sg_t']
     update_progression = [[0], [9], [4]]  # [[0],[5],[4],[7]] #[[4],[0,4,5]]
     model_save_weights = [0.0, 0.0, 1.0, 0.0, 1.0]
@@ -204,7 +208,7 @@ def kl_combo_boi():
 def coordinate_train(_run, mdp_map, gamma, alpha, beta1, beta2, constraint_batch_size, q_n_layers, q_layer_size, q_activation,
             q_output_activation, q_layer_norm, target_update_freq, dyn_n_layers, dyn_layer_size, dyn_activation,
             dyn_output_activation, dyn_layer_norm, weight_norm, boltz_beta, mellowmax, lse_softmax, gamma_demo, temp_boltz_beta, n_demos,
-            demo_time_steps, n_training_iters, dyn_pretrain_iters, batch_size, horizons, alphas, improvement_proportions,
+            demo_time_steps, n_training_iters, dyn_pretrain_iters, batch_size, horizons, alphas, improvement_proportions, kl_ball_schedule,
             switch_frequency, initial_update, update_progression, model_save_weights, tab_save_freq, clip_global, gpu_num, seed):
 
     os_setup(gpu_num)
@@ -243,7 +247,8 @@ def coordinate_train(_run, mdp_map, gamma, alpha, beta1, beta2, constraint_batch
                          'update_progression':update_progression,
                          'model_save_weights': model_save_weights,
                          'alphas': alphas,
-                         'clip_global':clip_global}
+                         'clip_global':clip_global,
+                         'kl_ball_schedule': kl_ball_schedule}
 
         model.initialize_training_regime("coordinate", regime_params=regime_params)
 

utils/models.py
@@ -229,8 +229,9 @@ class InverseDynamicsLearner():
         self.lqsafer = self.neg_avg_act_log_likelihood / (1/self.td_err + 1/self.neg_avg_trans_log_likelihood)
         self.llt_weighted_td_err = self.td_err_sgq * self.neg_avg_trans_log_likelihood #(1 + self.td_err_sgq) * (1 + self.neg_avg_trans_log_likelihood) ** 5
         self.lla_weighted_td_err = self.td_err_sgt * self.neg_avg_act_log_likelihood
-        self.kl_weighted_td_err = self.td_err_sgq * tf.maximum(self.bidirectional_KL, 1e-7)
-        self.kl_combo = self.td_err * tf.maximum(self.bidirectional_KL, 1e-7) * self.neg_avg_act_log_likelihood
+        self.kl_ball_ph = tf.placeholder(tf.float32, (), name='kl_ball')
+        self.kl_weighted_td_err = self.td_err_sgq * tf.maximum(self.bidirectional_KL, self.kl_ball_ph)
+        self.kl_combo = self.td_err * tf.maximum(self.bidirectional_KL, self.kl_ball_ph) * self.neg_avg_act_log_likelihood
         # self.td_err_weighted_ll = 1/(self.ll_weighted_td_err + 1e-8)
 
         # Set up target network
@@ -272,6 +273,8 @@ class InverseDynamicsLearner():
         self.log_loss_titles = self.loss_fns_titles[[0,1,2,5,8]]
 
         self.regime = None
+        self.validation_freq = None
+        self.kl_ball_schedule = None
 
 
     def initialize_training_regime(self, regime, regime_params, validation_freq=20):
@@ -283,6 +286,7 @@ class InverseDynamicsLearner():
 
         self.regime = regime
         self.validation_freq = validation_freq
+        self.kl_ball_schedule = regime_params.get('kl_ball_schedule', None)
 
         if regime == "weighted":
             losses = regime_params['losses']
@@ -460,6 +464,10 @@ class InverseDynamicsLearner():
                 demo_batch = sample_batch(rollouts, train_idxes, batch_size)
                 feed_dict = self._get_feed_dict(demo_batch, constraints, true_qs=true_qs)
 
+                if self.kl_ball_schedule is not None:
+                    feed_dict[self.kl_ball_ph] = self.kl_ball_schedule(train_time)
+                    val_feed[self.kl_ball_ph] = self.kl_ball_schedule(train_time)
+
                 if self.regime == "weighted":
                     # loss_data = self.sess.run(list(self.log_losses) + [self.update], feed_dict=feed_dict)[:-1]
                     self.sess.run(self.update, feed_dict=feed_dict)
@@ -580,19 +588,19 @@ class InverseDynamicsLearner():
 
         if self.prev_bests[self.curr_update_index] == np.inf:
             self.prev_bests[self.curr_update_index] = (sum(losses[:running_dist])/running_dist)
-            return False
-
 
         recent_loss = (sum(losses[-running_dist:])/running_dist)
-        improvement = 1 - recent_loss/(self.prev_bests[self.curr_update_index] + 1e-12)
-        switch = improvement > self.improvement_proportions[self.curr_update_index]
+        improvement = self.prev_bests[self.curr_update_index] / (recent_loss + 1e-12) - 1
+        switch = improvement < self.improvement_proportions[self.curr_update_index]
 
         if switch:
-            self.prev_bests[self.curr_update_index] = recent_loss
+            self.prev_bests[self.curr_update_index] = np.inf
             self.curr_update_index = (self.curr_update_index + 1) % len(self.update_progression)
             print("Loss improvement at {}, switching to loss config {}".format(improvement, self.curr_update_index))
             self.curr_loss = self.loss_progression[self.curr_update_index]
             self.curr_update = self.update_progression[self.curr_update_index]
+        else:
+            self.prev_bests[self.curr_update_index] = recent_loss
 
         return switch
 

utils/tab_learning_utils.py
@@ -97,7 +97,7 @@ def tabsoftq_T_grad_iter(mdp, T_thetas, Q, R, gamma, boltzmann=10000, maxiter=50
 
     T = mdp.adt_trans_to_sas_trans(Tps)
 
-    V = logsumexp(Q * boltzmann, axis=1) * boltzmann
+    V = logsumexp(Q * boltzmann, axis=1) / boltzmann
     T_grad = np.zeros((nS, nA, T_theta_dim))
     for s in range(nS):
         t = mdp.get_tile_type(s)
@@ -147,7 +147,7 @@ def eval_trans_likelihood(Tps, adt_obs):
     ll = 0.0
     for obs in adt_obs:
         a, d, t, = obs
-        l = np.log(Tps[a, d, t] + 1e-12)
+        l = np.log(Tps[a, d, t])
         ll += l
     return ll
 